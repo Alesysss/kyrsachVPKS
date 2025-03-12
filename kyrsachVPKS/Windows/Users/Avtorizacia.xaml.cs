06b569bfed52657c1dc7365f8800181f5f25f789
Alesys <ales_101@internet.net>
2025-03-12 13:26:28 +0300

    изменения в авторизации

--- kyrsachVPKS/Windows/Users/Avtorizacia.xaml.cs
+++ kyrsachVPKS/Windows/Users/Avtorizacia.xaml.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace kyrsachVPKS.Windows.Users
 {
@@ -35,12 +37,27 @@ namespace kyrsachVPKS.Windows.Users
             registracia.Show();
             this.Close(); 
         }
+        private bool ValidateFields()
+        {
+            Regex loginRegex = new Regex(@"^[A-Za-z0-9]{1,20}$");
+            Regex passwordRegex = new Regex(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{1,20}$");
 
+            void ShowError(Control field, string message)
+            {
+                MessageBox.Show(message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                field.Focus();
+            }
+            if (!loginRegex.IsMatch(LogTB.Text)) { ShowError(LogTB, "Ошибка в логине."); return false; }
+            if (!passwordRegex.IsMatch(PassTB.Text))
+            { ShowError(PassTB, "Ошибка в пароле."); return false; }
+
+            return true;
+        }
         private void Avtoriz_Click(object sender, RoutedEventArgs e)
         {
             string login = LogTB.Text;
             string password = PassTB.Text;
-
+            if (!ValidateFields()) return;
             using (ITschoolEntities db = new ITschoolEntities())
             {
                 var user = db.Accounts.FirstOrDefault(a => a.Login == login && a.Password == password);
